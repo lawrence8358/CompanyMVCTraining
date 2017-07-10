e9e2996a359ae0b865f244f4943fd4510a235466
Lawrence Shen <lawrence8358@hotmail.com>
2017-07-10 12:05:15 +0800

    1. 關注點方離，把View該處理的東西交給View自行處理。 2. 若View為共用的，就建立在共用Shared內。

--- 20170703MVC/Controllers/ARController.cs
+++ 20170703MVC/Controllers/ARController.cs
@@ -28,5 +28,19 @@ namespace _20170703MVC.Controllers
         {
             return "測試Unicode";
         }
+
+        //回傳訊息錯誤方式
+        public string ContentAlert1()
+        {
+            return "<script>alert('找不到網頁');history.back(-1);</script>";
+        }
+
+        //回傳訊息正確方式
+        public ActionResult ContentAlert2()
+        {
+            //關注點方離，把View該處理的東西交給View自行處理。
+            //若View為共用的，就建立在共用Shared內
+            return View("GoBack");
+        }
     }
 }
