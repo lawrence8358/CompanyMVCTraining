d3e91235ec324b842ae17d3e85cad16f55b15901
Lawrence Shen <lawrence8358@hotmail.com>
2017-07-17 15:38:45 +0800

    Parial 和 Action的使用時機。 1. Partial使用情境，已經可以明確的取得Model時使用。 2. 若無法Model內取得資料，建議使用Action的方式來取得該頁面的資料。

---
+++ 20170703MVC/Controllers/OrderLinesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _20170703MVC.Models;
+
+namespace _20170703MVC.Controllers
+{
+    public class OrderLinesController : Controller
+    {
+        private FabricsEntities db = new FabricsEntities();
+
+        // GET: OrderLines
+        public ActionResult Index(int id)
+        {
+            var orderLine = db.OrderLine.Where(p => p.ProductId.Equals(id)).Include(o => o.Order).Include(o => o.Product);
+            return View(orderLine.Take(3).ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
