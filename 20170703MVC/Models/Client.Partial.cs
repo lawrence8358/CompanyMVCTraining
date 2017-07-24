e97435e73fa59912cdc9575da4595ee69be201b8
Lawrence Shen <lawrence8358@hotmail.com>
2017-07-24 09:59:55 +0800

    指定特定型別的Template(處理日期中文檔名的問題)

--- 20170703MVC/Models/Client.Partial.cs
+++ 20170703MVC/Models/Client.Partial.cs
@@ -3,53 +3,57 @@ namespace _20170703MVC.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(ClientMetaData))]
     public partial class Client
     {
     }
-    
+
     public partial class ClientMetaData
     {
         [Required]
         public int ClientId { get; set; }
-        
-        [StringLength(40, ErrorMessage="欄位長度不得大於 40 個字元")]
+
+        [StringLength(40, ErrorMessage = "欄位長度不得大於 40 個字元")]
         public string FirstName { get; set; }
-        
-        [StringLength(40, ErrorMessage="欄位長度不得大於 40 個字元")]
+
+        [StringLength(40, ErrorMessage = "欄位長度不得大於 40 個字元")]
         public string MiddleName { get; set; }
-        
-        [StringLength(40, ErrorMessage="欄位長度不得大於 40 個字元")]
+
+        [StringLength(40, ErrorMessage = "欄位長度不得大於 40 個字元")]
         public string LastName { get; set; }
-        
-        [StringLength(1, ErrorMessage="欄位長度不得大於 1 個字元")]
+
+        [StringLength(1, ErrorMessage = "欄位長度不得大於 1 個字元")]
         public string Gender { get; set; }
+
+        [DataType(DataType.Date)] //套用Date Template
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] //使用此方式變成所有Date欄位都必須要這麼做，改成Date Template取代
         public Nullable<System.DateTime> DateOfBirth { get; set; }
+
         public Nullable<double> CreditRating { get; set; }
-        
-        [StringLength(7, ErrorMessage="欄位長度不得大於 7 個字元")]
+
+        [StringLength(7, ErrorMessage = "欄位長度不得大於 7 個字元")]
         public string XCode { get; set; }
         public Nullable<int> OccupationId { get; set; }
-        
-        [StringLength(20, ErrorMessage="欄位長度不得大於 20 個字元")]
+
+        [StringLength(20, ErrorMessage = "欄位長度不得大於 20 個字元")]
         public string TelephoneNumber { get; set; }
-        
-        [StringLength(100, ErrorMessage="欄位長度不得大於 100 個字元")]
+
+        [StringLength(100, ErrorMessage = "欄位長度不得大於 100 個字元")]
         public string Street1 { get; set; }
-        
-        [StringLength(100, ErrorMessage="欄位長度不得大於 100 個字元")]
+
+        [StringLength(100, ErrorMessage = "欄位長度不得大於 100 個字元")]
         public string Street2 { get; set; }
-        
-        [StringLength(100, ErrorMessage="欄位長度不得大於 100 個字元")]
+
+        [StringLength(100, ErrorMessage = "欄位長度不得大於 100 個字元")]
         public string City { get; set; }
-        
-        [StringLength(15, ErrorMessage="欄位長度不得大於 15 個字元")]
+
+        [StringLength(15, ErrorMessage = "欄位長度不得大於 15 個字元")]
         public string ZipCode { get; set; }
         public Nullable<double> Longitude { get; set; }
         public Nullable<double> Latitude { get; set; }
         public string Notes { get; set; }
-    
+
         public virtual Occupation Occupation { get; set; }
         public virtual ICollection<Order> Order { get; set; }
     }
